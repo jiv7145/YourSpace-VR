bb1386001acf427d57db434ac3d95f352bde3bdd
CHAEEUN KIM <ckim136@my.bcit.ca>
2020-05-18 02:42:53 -0700

    linked admin view

--- yourspace/codei/application/controllers/Fullcalendar.php
+++ yourspace/codei/application/controllers/Fullcalendar.php
@@ -8,22 +8,24 @@ use PHPMailer\PHPMailer\Exception;
 
 require '../vendor/autoload.php';
 class Fullcalendar extends CI_Controller {
+  
 
  public function __construct()
  {
   parent::__construct();
   $this->load->model('fullcalendar_model');
-  
  }
 
  function index()
  {
-  $this->load->view('fullcalendar');
+  $this->load->view('Fullcalendar');
  }
 
  function load()
  {
-  $event_data = $this->fullcalendar_model->fetch_all_event();
+  $name = $_SESSION['username'];
+  $email = $_SESSION['emailaddress'];
+  $event_data = $this->fullcalendar_model->fetch_all_event($email);
   foreach($event_data->result_array() as $row)
   {
    $data[] = array(
@@ -38,16 +40,41 @@ class Fullcalendar extends CI_Controller {
 
  function insert()
  {
+  $name = $_SESSION['username'];
+  $email = $_SESSION['emailaddress'];
   if($this->input->post('title'))
   {
+   
+
    $data = array(
     'title'  => $this->input->post('title'),
     'start_event'=> $this->input->post('start'),
-    'end_event' => $this->input->post('end')
+    'end_event' => $this->input->post('end'),
+    'admin'=>('False'),
+    'name'=>implode($name),
+    'email' =>$email
    );
    $this->fullcalendar_model->insert_event($data);
 
-   $mail = new PHPMailer(true);
+
+
+  }
+ }
+
+ function update()
+ {
+  $name = implode($_SESSION['username']);
+  if($this->input->post('id'))
+  {
+   $data = array(
+    'title'   => $this->input->post('title'),
+    'start_event' => $this->input->post('start'),
+    'end_event'  => $this->input->post('end')
+   );
+
+   $this->fullcalendar_model->update_event($data, $this->input->post('id'));
+  }
+     $mail = new PHPMailer(true);
 
 try {
     //Server settings
@@ -59,15 +86,21 @@ try {
     $mail->Password   = 'ccfc97954dace942efcd0e0d9d4842c9-3e51f8d2-5dacb6e5';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-    $name = implode($_SESSION['username']);
+   
     //Recipients
     $mail->setFrom('postmaster@sandboxaea20dd704434c90afa8bb9243767d46.mailgun.org', 'Yourspace');
     $mail->addAddress('devel4800test@gmail.com', $name);     // Add a recipient
    
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Booking confirmation';
-    $mail->Body    = "Hi $name,<br> You have successfully booked an appointment";
+    $mail->Subject = 'Booking Update';
+   
+    $body = "Hi $name, You have updated your appointment <br>";
+     $body   .= "Start: ";
+     $body .= $this->input->post('start');
+    $body   .="<br> End: ";
+    $body .=$this->input->post('end');
+    $mail->Body  = $body;
    
 
     $mail->send();
@@ -76,28 +109,110 @@ try {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
 
-  }
- }
+$mailadmin = new PHPMailer(true);
 
- function update()
- {
-  if($this->input->post('id'))
-  {
-   $data = array(
-    'title'   => $this->input->post('title'),
-    'start_event' => $this->input->post('start'),
-    'end_event'  => $this->input->post('end')
-   );
+try {
+    //Server settings
+    $mailadmin->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mailadmin->isSMTP();                                            // Send using SMTP
+    $mailadmin->Host       = 'smtp.mailgun.org';                    // Set the SMTP server to send through
+    $mailadmin->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mailadmin->Username   = 'postmaster@sandboxaea20dd704434c90afa8bb9243767d46.mailgun.org';                     // SMTP username
+    $mailadmin->Password   = 'ccfc97954dace942efcd0e0d9d4842c9-3e51f8d2-5dacb6e5';                               // SMTP password
+    $mailadmin->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mailadmin->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+   
+    //Recipients
+    $mailadmin->setFrom('postmaster@sandboxaea20dd704434c90afa8bb9243767d46.mailgun.org', 'Yourspace');
+    $mailadmin->addAddress('devel4800test@gmail.com', $name);     // Add a recipient
+   
+    // Content
+    $mailadmin->isHTML(true);                                  // Set email format to HTML
+    $mailadmin->Subject = 'Booking Update';
+   
+    $body = "Appointment has been updated <br>";
+    $body   .=  "Name: $name <br>";
+     $body   .= "Start: ";
+     $body .= $this->input->post('start');
+    $body   .="<br> End: ";
+    $body .=$this->input->post('end');
+    $mailadmin->Body  = $body;
+   
 
-   $this->fullcalendar_model->update_event($data, $this->input->post('id'));
-  }
+    $mailadmin->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
  }
 
  function delete()
  {
+  $name = implode($_SESSION['username']);
   if($this->input->post('id'))
   {
    $this->fullcalendar_model->delete_event($this->input->post('id'));
+  }
+  $mail2 = new PHPMailer(true);
+
+  try {
+      //Server settings
+      $mail2->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+      $mail2->isSMTP();                                            // Send using SMTP
+      $mail2->Host       = 'smtp.mailgun.org';                    // Set the SMTP server to send through
+      $mail2->SMTPAuth   = true;                                   // Enable SMTP authentication
+      $mail2->Username   = 'postmaster@sandboxaea20dd704434c90afa8bb9243767d46.mailgun.org';                     // SMTP username
+      $mail2->Password   = 'ccfc97954dace942efcd0e0d9d4842c9-3e51f8d2-5dacb6e5';                               // SMTP password
+      $mail2->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+      $mail2->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+     
+      //Recipients
+      $mail2->setFrom('postmaster@sandboxaea20dd704434c90afa8bb9243767d46.mailgun.org', 'Yourspace');
+      $mail2->addAddress('devel4800test@gmail.com', $name);     // Add a recipient
+     
+      // Content
+      $mail2->isHTML(true);                                  // Set email format to HTML
+      $mail2->Subject = 'Booking Deletion';
+     
+      $body = "Hi $name, this is to confirm that your appointment has been cancelled";
+      $mail2->Body  = $body;
+     
+  
+      $mail2->send();
+      echo 'Message has been sent';
+  } catch (Exception $e) {
+      echo "Message could not be sent. Mailer Error: {$mail2->ErrorInfo}";
+  }
+
+  $mail2Admin = new PHPMailer(true);
+
+  try {
+      //Server settings
+      $mail2Admin->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+      $mail2Admin->isSMTP();                                            // Send using SMTP
+      $mail2Admin->Host       = 'smtp.mailgun.org';                    // Set the SMTP server to send through
+      $mail2Admin->SMTPAuth   = true;                                   // Enable SMTP authentication
+      $mail2Admin->Username   = 'postmaster@sandboxaea20dd704434c90afa8bb9243767d46.mailgun.org';                     // SMTP username
+      $mail2Admin->Password   = 'ccfc97954dace942efcd0e0d9d4842c9-3e51f8d2-5dacb6e5';                               // SMTP password
+      $mail2Admin->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+      $mail2Admin->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+     
+      //Recipients
+      $mail2Admin->setFrom('postmaster@sandboxaea20dd704434c90afa8bb9243767d46.mailgun.org', 'Yourspace');
+      $mail2Admin->addAddress('devel4800test@gmail.com', $name);     // Add a recipient
+     
+      // Content
+      $mail2Admin->isHTML(true);                                  // Set email format to HTML
+      $mail2Admin->Subject = 'Booking Deletion';
+     
+      $body = "An appointmnent has been deleted, please check the calendar";
+      $mail2Admin->Body  = $body;
+     
+  
+      $mail2Admin->send();
+      echo 'Message has been sent';
+  } catch (Exception $e) {
+      echo "Message could not be sent. Mailer Error: {$mail2->ErrorInfo}";
   }
  }
 
--- yourspace/public/php/surveyProcessor.php
+++ yourspace/public/php/surveyProcessor.php
@@ -1,9 +1,18 @@
 <?php
+
 session_start();
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require '../../vendor/autoload.php';
+
 
 $con = mysqli_connect('remotemysql.com', 'SKIR56Zums', 'JioDYRliuC');
 mysqli_select_db($con,'SKIR56Zums');
 
+// $con = mysqli_connect('localhost', 'root', '');
+// mysqli_select_db($con,'form');
 
 $email = $_SESSION['emailaddress'];
 $age = $_POST['age'];
@@ -17,24 +26,97 @@ $reference = $_POST['reference'];
 $counselling_exp = $_POST['counselling_exp'];
 $goal = $_POST['goal'];
 
+$date = $_POST['date'];
+$start = $_POST['start'];
+$end = $_POST['end'];
 
 
-$s = " select * from usertable where email = '$email'";
-$result = mysqli_query($con, $s);
-$num = mysqli_num_rows($result);
-if($num == 1){
-
-    $reg = "insert into usersurvey(email, age, gender
+    $reg = "insert into form(email, age, gender
     ,relationship_status, sexual_orientation, language, 
-    employment_situation,reference, counselling_exp,goal  ) 
+    employment_situation,reference, counselling_exp,goal, date, start, end) 
     values ('$email', '$age', '$gender'
     , '$relationship_status', '$sexual_orientation', '$language', '$employment_situation'
-    , '$reference', '$counselling_exp', '$goal')";
-   mysqli_query($con, $reg);
-   header('location:home.php');
+    , '$reference', '$counselling_exp', '$goal','$date','$start','$end')";
+    if(mysqli_query($con, $reg)){
+    }else{
+     
+        echo "failed";
+    }
 
-}else{
-    
-}
+
+
+$mail = new PHPMailer(true);
+$name =  implode($_SESSION['username']);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.mailgun.org';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'postmaster@sandboxaea20dd704434c90afa8bb9243767d46.mailgun.org';                     // SMTP username
+    $mail->Password   = 'ccfc97954dace942efcd0e0d9d4842c9-3e51f8d2-5dacb6e5';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
    
+    //Recipients
+    $mail->setFrom('postmaster@sandboxaea20dd704434c90afa8bb9243767d46.mailgun.org', 'Yourspace');
+    $mail->addAddress('devel4800test@gmail.com', "admin");     // Add a recipient
+   
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Booking Details';
+    $body   = " $name have booked a new appointment <br>";
+    $body   .= "Date:  $date <br>";
+    $body   .= "Start: $start <br>";
+    $body   .="End:  $end <br>";
+    $body    .= "Age:  $age <br>";
+    $body    .= "Gender: $gender <br>";
+    $body   .="Relationship Status: $relationship_status <br>";
+    $body    .="Sexual Orientation: $sexual_orientation <br>";
+    $body    .="Primary Language:  $language <br>";
+    $body .= "Employment Situation:  $employment_situation <br>";
+    $body   .="Reference: $reference <br>";
+    $body   .="Couslling Experience:  $counselling_exp <br>";
+    $body   .="Couselling Goal: $goal <br>";
+    $mail->Body = $body;
+
+
+   $mail->send();
+   
+   header('location:home.php');
+} catch (Exception $e) {
+}
+
+
+$mail2 = new PHPMailer(true);
+try {
+    //Server settings
+    $mail2->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail2->isSMTP();                                            // Send using SMTP
+    $mail2->Host       = 'smtp.mailgun.org';                    // Set the SMTP server to send through
+    $mail2->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail2->Username   = 'postmaster@sandboxaea20dd704434c90afa8bb9243767d46.mailgun.org';                     // SMTP username
+    $mail2->Password   = 'ccfc97954dace942efcd0e0d9d4842c9-3e51f8d2-5dacb6e5';                               // SMTP password
+    $mail2->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail2->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+   
+    //Recipients
+    $mail2->setFrom('postmaster@sandboxaea20dd704434c90afa8bb9243767d46.mailgun.org', 'Yourspace');
+    $mail2->addAddress('devel4800test@gmail.com', "admin");     // Add a recipient
+   
+    // Content
+    $mail2->isHTML(true);                                  // Set email format to HTML
+    $mail2->Subject = 'Booking Confirmation';
+    $body   = "Hi $name, you have booked a new appointment <br>";
+  
+    $mail2->Body = $body;
+
+
+   $mail2->send();
+   } catch (Exception $e) {
+}
+
+
+    
 ?>

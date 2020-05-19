cfd7b16fb130f2d082e90f5529c100da52db67ad
ckim136 <kimemily1227@gmail.com>
2020-05-18 18:35:50 -0700

    Update start.php

--- yourspace/app/start.php
+++ yourspace/app/start.php
@@ -1,7 +1,7 @@
 <?php
 require '../vendor/autoload.php';
 
-define('SITE_URL', 'http://localhost/yourspace/payment');
+define('SITE_URL', 'http://yourspacecounselling.herokuapp.com/');
 
 $paypal = new \PayPal\Rest\ApiContext(
     new \PayPal\Auth\OAuthTokenCredential(

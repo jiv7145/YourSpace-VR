a4999b7062fff256b43365f8cf670108a10f5f80
ckim136 <kimemily1227@gmail.com>
2020-05-18 18:28:22 -0700

    paypal live account

--- yourspace/app/start.php
+++ yourspace/app/start.php
@@ -5,6 +5,6 @@ define('SITE_URL', 'http://localhost/yourspace/payment');
 
 $paypal = new \PayPal\Rest\ApiContext(
     new \PayPal\Auth\OAuthTokenCredential(
-        'ATEtfQ5lYVaqIgFUtzIQzSmohH3SAXlDRZmbV7rtB3gNIec-awM08xwnuzTDwBeyvP8orc74KY2FLBZG',
-        'EE5abSJwhbzw-ENhR9d4QAbmRd0Xb2_pNRCQQ8FP06nYGMoj75Mqw4VO7Xh_BbUnsUx4-8EEyMdXv85SeMTMQtnc2JX_i')
+        'yourspacecounselling@hotmail.com',
+        'AZcRzI643ONcfez6xV1b7cKNaAWPR-a78ie-5uvWzA0oScK-al_Cmjbn2FypSlJrQ9_6fKeERazRqldq')
     );
